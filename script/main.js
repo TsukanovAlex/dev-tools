d6e26cfe9c16911a3cf240a8c9ccc7444cd02eb7
*Aleksey <*atsukanov89@gmail.com*>
2023-07-18 22:15:29 +0300

    Реализована логика выбора сложности

--- script/main.js
+++ script/main.js
@@ -1,3 +1,25 @@
+
+import { renderGame } from "./components/render-game-component.js";
 let globalState = {
     level: '',
-}
+};
+
+const levelEl = document.querySelectorAll('.level-input');
+const startButtonEl = document.getElementById('start-button');
+
+
+levelEl.forEach(input => {
+    input.addEventListener('click', () => {
+        globalState.level = input.dataset.index;
+        console.log(globalState.level);
+    });
+});
+
+startButtonEl.addEventListener('click', () => {
+    
+    if (globalState.level) {
+        renderGame(globalState.level);
+    } else {
+        alert('Пожалуйста, выберите уровень сложности');
+    }
+});
