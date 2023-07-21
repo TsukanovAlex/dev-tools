a41149c356493a0413152ad18739097407585918
*Aleksey <*atsukanov89@gmail.com*>
2023-07-21 23:30:37 +0300

    Выполнено ДЗ №3.3

--- script/components/render-game-component.js
+++ script/components/render-game-component.js
@@ -1,17 +1,52 @@
-export function renderGame(level) {
-    const appEl = document.getElementById('main-box');
+export function renderGame() {
+    const appEl = document.getElementById('app')
 
-    const levels = {
-        '1': 'легкий',
-        '2': 'средний',
-        '3': 'сложный'
-    };
-    let selectedLevel = levels[level];
-
+    const cards = '<div class="cards-of-shirt"></div>'.repeat(36)
     const appHtml = `
-    <div class="main">
-                <p class="selected-level">Отлично!!!<br><br>Вы выбрали <span class="text-level-color">${selectedLevel}</span> уровень</p>
-            </div>`
+        <header class="header center">
+            <div class="timer"></div>
+            <button class="header-game-button" id="showCardButton">Показать карты</button>
+            <button class="header-game-button" id="startNewGameButton">Начать заново</button>
+        </header>
+        <section class="center game-box">${cards}</section>
+    `
 
-    appEl.innerHTML = appHtml;
-};
+    appEl.innerHTML = appHtml
+
+    document
+        .getElementById('startNewGameButton')
+        .addEventListener('click', () => {
+            console.log('restart')
+            renderGame()
+        })
+    document.getElementById('showCardButton').addEventListener('click', () => {
+        renderOpenedCard()
+    })
+}
+// Функция показа карт
+function renderOpenedCard() {
+    const appEl = document.getElementById('app')
+    let cardItems = ''
+    for (let i = 1; i <= 36; i++) {
+        cardItems += `<div class="cards-of-shirt card-item${i}"></div>`
+    }
+    const openCardHtml = `
+            <header class="center header">
+                <div class="timer"></div>
+                <button class="header-game-button" id="showCardButton">Скрыть карты</button>
+                <button class="header-game-button" id="startNewGameButton">Начать заново</button>
+            </header>
+            <section class="center game-box">${cardItems}</section>
+        `
+    appEl.innerHTML = openCardHtml
+
+    document
+        .getElementById('startNewGameButton')
+        .addEventListener('click', () => {
+            console.log('restart')
+            renderGame()
+        })
+    document.getElementById('showCardButton').addEventListener('click', () => {
+        renderOpenedCard()
+    })
+}
--- script/main.js
+++ script/main.js
@@ -1,26 +1,68 @@
-
-import { renderGame } from "./components/render-game-component.js";
+import { renderGame } from './components/render-game-component.js'
 
 let globalState = {
     level: '',
-};
+}
 
-const levelEl = document.querySelectorAll('.level-input');
-const startButtonEl = document.getElementById('start-button');
+// рендер страницы с выбором уровня сложности
+export const renderPageChangeLevel = () => {
+    const appEl = document.getElementById('app')
+    const appHtml = `<div class="main" id="main-box">
+    <h2 class="main__title">Выбери сложность</h2>
+    <div class="main__level-box">
+        <input
+            type="radio"
+            name="games"
+            data-index="1"
+            id="level-light"
+            class="level-input"
+            value="1"
+        />
+        <label for="level-light">1</label>
+
+        <input
+            type="radio"
+            name="games"
+            data-index="2"
+            id="level-medium"
+            class="level-input"
+            value="2"
+        />
+        <label for="level-medium">2</label>
+
+        <input
+            type="radio"
+            name="games"
+            data-index="3"
+            id="level-hard"
+            class="level-input"
+            value="3"
+        />
+        <label for="level-hard">3</label>
+    </div>
+    <button id="start-button" class="main__button">Старт</button>
+</div>`
+    appEl.innerHTML = appHtml
+}
+
+renderPageChangeLevel()
+
+const levelEl = document.querySelectorAll('.level-input')
+const startButtonEl = document.getElementById('start-button')
 
 // Обработчик клика на  все инпуты выбора уровня
-levelEl.forEach(input => {
+levelEl.forEach((input) => {
     input.addEventListener('click', () => {
-        globalState.level = input.dataset.index;
-        console.log(globalState.level);
-    });
-});
+        globalState.level = input.dataset.index
+        console.log(globalState.level)
+    })
+})
 
-// Обработчик клика на кнопку старт 
+// Обработчик клика на кнопку старт
 startButtonEl.addEventListener('click', () => {
     if (globalState.level) {
-        renderGame(globalState.level);
+        renderGame(globalState.level)
     } else {
-        alert('Пожалуйста, выберите уровень сложности');
+        alert('Пожалуйста, выберите уровень сложности')
     }
-});
+})
